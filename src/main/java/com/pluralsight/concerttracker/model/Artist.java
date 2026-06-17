ccc9e861706e47d040afabf7cad1e9a1184bd60c
gwamaka1 <mwamtobegwamaka@gmail.com>
2026-06-17 10:25:24 -0400

    did artist class

---
+++ src/main/java/com/pluralsight/concerttracker/model/Artist.java
@@ -0,0 +1,46 @@
+package com.pluralsight.concerttracker.model;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "artists")
+public class Artist {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private String genre;
+
+    public Artist() {
+    }
+
+    public Artist(String name, String genre) {
+        this.name = name;
+        this.genre = genre;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+    public void setGenre(String genre) {
+        this.genre = genre;
+    }
+}
